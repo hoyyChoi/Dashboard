300fe53815e80ad6d7b5b2511d2e0a4b0cfe4637
hoyyChoi <hoyeon8165@gmail.com>
2023-03-07 18:17:58 +0900

    [feat] #2 - Login_Page(Routing)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { useState } from 'react';
+import { Routes,Route, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import MainPage from './components/MainPage';
+
 
 function App() {
+
+  const [auth,setAuth] = useState(false)
+
+  const PrivateRoute = () =>{
+    return auth ? <MainPage/> : <Navigate to="/login"/>
+  }
+
+  
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<PrivateRoute/>}/>
+        <Route path="/login" element={<Login setAuth={setAuth}/>}/>
+      </Routes>
     </div>
   );
 }
