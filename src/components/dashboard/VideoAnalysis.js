4c689086454091270e640039b476b16c193c1e08
hoyyChoi <hoyeon8165@gmail.com>
2023-03-24 21:36:53 +0900

    충돌해결

--- src/components/dashboard/VideoAnalysis.js
+++ src/components/dashboard/VideoAnalysis.js
@@ -10,9 +10,6 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 const VideoAnalysis = ({selected,setSelected,btn,setBtn,analy,sum,btn1}) => {
 
- 
-  
-
 
     const handleSelect = (e) => {
 
@@ -142,57 +139,57 @@ export default VideoAnalysis
 
 
             //진료과별
-export const dataDepartment = {
+// export const dataDepartment = {
 
-  labels: ['피부과', '내과', '성형외과','외과', '이비인후과', '기타'],
-  datasets: [
-    {
-      data: [30,22,20,14,10,4],
-      backgroundColor: [
-        'rgba(51, 97, 255, 1.0)',
-        'rgba(51, 97, 255, 0.8)',
-        'rgba(51, 97, 255, 0.6)',
-        'rgba(51, 97, 255, 0.4)',
-        'rgba(51, 97, 255, 0.2)',
-        'rgba(107,122,153, 0.9)'
-      ],
-      borderColor: [
-        'rgba(51, 97, 255, 1.0)',
-        'rgba(51, 97, 255, 0.8)',
-        'rgba(51, 97, 255, 0.6)',
-        'rgba(51, 97, 255, 0.4)',
-        'rgba(51, 97, 255, 0.2)',
-        'rgba(107,122,153, 0.9)'
-      ],
-      borderWidth: 1
-    },
-  ],
-};
+//   labels: ['피부과', '내과', '성형외과','외과', '이비인후과', '기타'],
+//   datasets: [
+//     {
+//       data: [30,22,20,14,10,4],
+//       backgroundColor: [
+//         'rgba(51, 97, 255, 1.0)',
+//         'rgba(51, 97, 255, 0.8)',
+//         'rgba(51, 97, 255, 0.6)',
+//         'rgba(51, 97, 255, 0.4)',
+//         'rgba(51, 97, 255, 0.2)',
+//         'rgba(107,122,153, 0.9)'
+//       ],
+//       borderColor: [
+//         'rgba(51, 97, 255, 1.0)',
+//         'rgba(51, 97, 255, 0.8)',
+//         'rgba(51, 97, 255, 0.6)',
+//         'rgba(51, 97, 255, 0.4)',
+//         'rgba(51, 97, 255, 0.2)',
+//         'rgba(107,122,153, 0.9)'
+//       ],
+//       borderWidth: 1
+//     },
+//   ],
+// };
 
-//질환별 
-export const dataDisease = {
-  labels: ['피부과', '내과', '성형외과','외과', '이비인후과', '기타'],
-  datasets: [
-    {
-      // label:' ',
-      data: [30,22,20,14,10,4],
-      backgroundColor: [
-        'rgba(51, 97, 255, 1.0)',
-        'rgba(51, 97, 255, 0.8)',
-        'rgba(51, 97, 255, 0.6)',
-        'rgba(51, 97, 255, 0.4)',
-        'rgba(51, 97, 255, 0.2)',
-        'rgba(107,122,153, 0.9)'
-      ],
-      borderColor: [
-        'rgba(51, 97, 255, 1.0)',
-        'rgba(51, 97, 255, 0.8)',
-        'rgba(51, 97, 255, 0.6)',
-        'rgba(51, 97, 255, 0.4)',
-        'rgba(51, 97, 255, 0.2)',
-        'rgba(107,122,153, 0.9)'
-      ],
-      borderWidth: 1
-    },
-  ],
-};
+// //질환별 
+// export const dataDisease = {
+//   labels: ['피부과', '내과', '성형외과','외과', '이비인후과', '기타'],
+//   datasets: [
+//     {
+//       // label:' ',
+//       data: [30,22,20,14,10,4],
+//       backgroundColor: [
+//         'rgba(51, 97, 255, 1.0)',
+//         'rgba(51, 97, 255, 0.8)',
+//         'rgba(51, 97, 255, 0.6)',
+//         'rgba(51, 97, 255, 0.4)',
+//         'rgba(51, 97, 255, 0.2)',
+//         'rgba(107,122,153, 0.9)'
+//       ],
+//       borderColor: [
+//         'rgba(51, 97, 255, 1.0)',
+//         'rgba(51, 97, 255, 0.8)',
+//         'rgba(51, 97, 255, 0.6)',
+//         'rgba(51, 97, 255, 0.4)',
+//         'rgba(51, 97, 255, 0.2)',
+//         'rgba(107,122,153, 0.9)'
+//       ],
+//       borderWidth: 1
+//     },
+//   ],
+// };
