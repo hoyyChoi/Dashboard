6cf146cbc9c4a040b81f2c408b0c80c3c74fe337
hoyyChoi <hoyeon8165@gmail.com>
2023-03-24 23:08:02 +0900

    [mod] upload 오류 수정  및 메모 부분 이미지 변경

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -11,23 +11,23 @@ const Manager = () => {
   const [open,setOpen] = useState(false)
   const [appear,setAppear] = useState(true)
   const [memolist,setMemolist] = useState([
-    {img:'Mask group.svg',
-     name:'최호연',
+    {img:'Connector1.png',
+     name:'신형만',
      time:'1시간전',
      num:3,
      memo:'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.'
     },
-    {img:'Mask group.svg',
-     name:'최호연2',
+    {img:'Connector2.png',
+     name:'봉미선',
      time:'2시간전',
      num:2,
-     memo:'오늘 오전 중으로 자료 넘겨드릴게'
+     memo:'오늘 오전 중으로 자료 넘겨드릴게 '
     },
-    {img:'Mask group.svg',
-     name:'최호연3',
+    {img:'Connector3.png',
+     name:'신짱구',
      time:'2시간전',
      num:0,
-     memo:'어쩌고 저쩌고 연락드릴'}
+     memo:'어쩌고 저쩌고 해서 연락드립니다!!!!'}
     ])
   const [memoCount,setMemoCount] = useState(memolist.length)
 
--- src/components/manager1/ManagerInfo.js
+++ src/components/manager1/ManagerInfo.js
@@ -9,7 +9,7 @@ const ManagerInfo = () => {
         </div>
         <div className='img'>
             <div style={{width:'100px'}}><img style={{marginLeft:'68px'}} width={32} src='Mask group.svg'/></div>
-            <div style={{fontSize:'12px',fontWeight:'500',color:'#6B7A99',lineHeight:'24px'}}>Vertified Account</div>
+            <div style={{fontSize:'12px',fontWeight:'500',color:'#6B7A99',lineHeight:'24px'}}>Verified Account</div>
         </div>
     </div>
   )
