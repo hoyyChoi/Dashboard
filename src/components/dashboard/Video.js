631371f2c4c4d735875007ba2fec16723d9d2f05
hoyyChoi <hoyeon8165@gmail.com>
2023-03-24 21:23:10 +0900

    [mod] 비디오& 라이브 차트변경 및 서버연결

--- src/components/dashboard/Video.js
+++ src/components/dashboard/Video.js
@@ -1,27 +1,264 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import VideoView from './VideoView'
 import VideoAnalysis from './VideoAnalysis'
+import {getTopdepartment,getTopdisease,getNonmedical,getLiveTopdepartment,getLiveTopdisease} from '../../remote/server'
 
 const Video = () => {
 
-    const [btn,setBtn] = useState(false)
+    const [btn1,setBtn1] = useState(false)
+    const [selected, setSelected] = useState('진료');
+    const [btn,setBtn] = useState(1);
+    const [sum,setSum] = useState(0);
+    const [analy,setAnaly] = useState({label:[],datasets:[]})
+    const [data,setData] = useState([])
+
+
+    let sub = []
+  let hit = []
+  let sumvalue = 0
+
+
+  useEffect(()=>{
+    if(btn1){
+        if(btn === 1){
+            getLiveTopdepartment()
+            .then(res=>{
+            const arr = res.data
+            console.log('111',res.data)
+            arr.map((item)=>{
+                sub.push(item.subject)
+                hit.push(item.hits)
+            })
+            setData([{
+                data: hit,
+                backgroundColor: [
+                'rgba(51, 97, 255, 1.0)',
+                'rgba(51, 97, 255, 0.8)',
+                'rgba(51, 97, 255, 0.6)',
+                'rgba(51, 97, 255, 0.4)',
+                'rgba(51, 97, 255, 0.2)',
+                'rgba(107,122,153, 0.9)'
+                ],
+                borderColor: [
+                'rgba(51, 97, 255, 1.0)',
+                'rgba(51, 97, 255, 0.8)',
+                'rgba(51, 97, 255, 0.6)',
+                'rgba(51, 97, 255, 0.4)',
+                'rgba(51, 97, 255, 0.2)',
+                'rgba(107,122,153, 0.9)'
+                ],
+                borderWidth: 1
+            }])
+                hit.map((item)=>{
+                sumvalue += parseFloat(item)
+                })
+                setSum(Math.ceil(sumvalue * 100) / 100)
+                setAnaly((analy)=>{
+                return{
+                    labels:sub,
+                    datasets:data
+                }
+                })
+            })
+            .catch((err)=>console.log(err));
+        }else if(btn === 2){
+            getLiveTopdisease()
+            .then(res=>{
+            const arr = res.data
+            console.log('111',res.data)
+            arr.map((item)=>{
+                sub.push(item.subject)
+                hit.push(item.hits)
+            })
+            setData([{
+                data: hit,
+                backgroundColor: [
+                'rgba(51, 97, 255, 1.0)',
+                'rgba(51, 97, 255, 0.8)',
+                'rgba(51, 97, 255, 0.6)',
+                'rgba(51, 97, 255, 0.4)',
+                'rgba(51, 97, 255, 0.2)',
+                'rgba(107,122,153, 0.9)'
+                ],
+                borderColor: [
+                'rgba(51, 97, 255, 1.0)',
+                'rgba(51, 97, 255, 0.8)',
+                'rgba(51, 97, 255, 0.6)',
+                'rgba(51, 97, 255, 0.4)',
+                'rgba(51, 97, 255, 0.2)',
+                'rgba(107,122,153, 0.9)'
+                ],
+                borderWidth: 1
+            }])
+                hit.map((item)=>{
+                sumvalue += parseFloat(item)
+                })
+                setSum(Math.ceil(sumvalue * 100) / 100)
+                setAnaly((analy)=>{
+                return{
+                    labels:sub,
+                    datasets:data
+                }
+                })
+            })
+            .catch((err)=>console.log(err));
+        }
+    }else{
+        if(selected === '진료'){
+            if(btn === 1){
+              getTopdepartment()
+              .then(res=>{
+                const arr = res.data
+                arr.map((item)=>{
+                  sub.push(item.subject)
+                  hit.push(item.hits)
+               })
+               
+              setData([{
+                data: hit,
+                backgroundColor: [
+                  'rgba(51, 97, 255, 1.0)',
+                'rgba(51, 97, 255, 0.8)',
+                'rgba(51, 97, 255, 0.6)',
+                'rgba(51, 97, 255, 0.4)',
+                'rgba(51, 97, 255, 0.2)',
+                'rgba(107,122,153, 0.9)'
+                ],
+                borderColor: [
+                  'rgba(51, 97, 255, 1.0)',
+                  'rgba(51, 97, 255, 0.8)',
+                  'rgba(51, 97, 255, 0.6)',
+                  'rgba(51, 97, 255, 0.4)',
+                  'rgba(51, 97, 255, 0.2)',
+                  'rgba(107,122,153, 0.9)'
+                ],
+                borderWidth: 1
+              }])
+                hit.map((item)=>{
+                  sumvalue += parseFloat(item)
+                })
+                setSum(Math.ceil(sumvalue * 100) / 100)
+                setAnaly((analy)=>{
+                  return{
+                    labels:sub,
+                    datasets:data
+                  }
+                })
+            })
+            .catch((err)=>console.log(err));
+          }else if(btn === 2){
+            getTopdisease()
+            .then(res=>{
+              const arr = res.data
+              arr.map((item)=>{
+                sub.push(item.subject)
+                hit.push(item.hits)
+             })
+             
+            setData([{
+              data: hit,
+              backgroundColor: [
+                'rgba(51, 97, 255, 1.0)',
+                'rgba(51, 97, 255, 0.8)',
+                'rgba(51, 97, 255, 0.6)',
+                'rgba(51, 97, 255, 0.4)',
+                'rgba(51, 97, 255, 0.2)',
+                'rgba(107,122,153, 0.9)'
+              ],
+              borderColor: [
+                'rgba(51, 97, 255, 1.0)',
+                'rgba(51, 97, 255, 0.8)',
+                'rgba(51, 97, 255, 0.6)',
+                'rgba(51, 97, 255, 0.4)',
+                'rgba(51, 97, 255, 0.2)',
+                'rgba(107,122,153, 0.9)'
+              ],
+              borderWidth: 1
+            }])
+              hit.map((item)=>{
+                sumvalue += parseFloat(item)
+              })
+              setSum(Math.ceil(sumvalue * 100) / 100)
+              setAnaly((analy)=>{
+                return{
+                  labels:sub,
+                  datasets:data
+                }
+              })
+          })
+          .catch((err)=>console.log(err));
+          }
+          }else{
+            getNonmedical()
+            .then(res=>{
+              const arr = res.data
+              arr.map((item)=>{
+                sub.push(item.subject)
+                hit.push(item.hits)
+             })
+             
+            setData([{
+              data: hit,
+              backgroundColor: [
+                'rgba(51, 97, 255, 1.0)',
+                'rgba(51, 97, 255, 0.8)',
+                'rgba(51, 97, 255, 0.6)',
+                'rgba(51, 97, 255, 0.4)',
+                'rgba(51, 97, 255, 0.2)',
+                'rgba(107,122,153, 0.9)'
+              ],
+              borderColor: [
+                'rgba(51, 97, 255, 1.0)',
+                'rgba(51, 97, 255, 0.8)',
+                'rgba(51, 97, 255, 0.6)',
+                'rgba(51, 97, 255, 0.4)',
+                'rgba(51, 97, 255, 0.2)',
+                'rgba(107,122,153, 0.9)'
+              ],
+              borderWidth: 1
+            }])
+              hit.map((item)=>{
+                sumvalue += parseFloat(item)
+              })
+              setSum(Math.ceil(sumvalue * 100) / 100)
+              setAnaly((analy)=>{
+                return{
+                  labels:sub,
+                  datasets:data
+                }
+              })
+          })
+          .catch((err)=>console.log(err));
+          }
+          
+      
+          
+    }
+  },[data,selected])
+    
   return (
     <div className='videoStatus'>
         <div className='status-header'>
             <header>
                 <div className='status-header-left'>
-                    <div className={btn?'video':'select'} onClick={() => setBtn(false)}>영상</div>
-                    <div className={btn?'select':'live'} onClick={() => setBtn(true)}>라이브</div>
+                    <div className={btn1?'video':'select'} onClick={() => setBtn1(false)}>영상</div>
+                    <div className={btn1?'select':'live'} onClick={() => setBtn1(true)}>라이브</div>
                 </div>
                 <div className='status-header-right'>
                     <div style={{fontSize:'14px',marginLeft:'4px',fontWeight:'500',lineHeight:'24px'}}>2023년 3월 기준</div>
                 </div>
             </header>
         </div>
-        <div className='video-card'>
-            <VideoView/>
-            <VideoAnalysis/>
-        </div>
+            {btn?
+            <div className='video-card'>
+                <VideoView/>
+                <VideoAnalysis selected={selected} setSelected={setSelected} btn={btn} setBtn={setBtn} analy={analy} sum={sum} btn1={btn1}/>
+            </div>
+            :
+            <div className='video-card'>
+                <VideoView/>
+                <VideoAnalysis selected={selected} setSelected={setSelected} btn={btn} setBtn={setBtn} analy={analy} sum={sum} btn1={btn1}/>
+            </div>}
     </div>
   )
 }
--- src/components/dashboard/VideoAnalysis.js
+++ src/components/dashboard/VideoAnalysis.js
@@ -1,18 +1,152 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2'
 import { Dropdown, Selection } from 'react-dropdown-now';
 import 'react-dropdown-now/style.css'
 
+
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-//진료과별
+
+const VideoAnalysis = ({selected,setSelected,btn,setBtn,analy,sum,btn1}) => {
+
+ 
+  
+
+
+    const handleSelect = (e) => {
+
+      if(e.value === '진료'){
+        setSelected('진료')
+      }else{
+        setSelected('비진료')
+      }
+  };
+
+  const selectButton = (num) =>{
+    setBtn(num)
+  }
+
+  
+
+  return (
+    <div className='video-analysis'>
+      <div className='title'>
+        <div className='analysis-title'>
+        조회수 분석
+        </div>
+        {!btn1?
+        <Dropdown className='info-dropdown'
+        options={['진료', '비진료']}
+        value={selected}
+        onChange={handleSelect}
+      />:""}
+      </div>
+
+
+      <div className='doughnut'>
+        <Doughnut 
+            data={analy}
+            options = {{
+              layout: {
+                padding: {
+                  left:20,
+                  right: -10,
+                  top: 0,
+                  bottom: 80
+              },
+              
+              },
+              
+              responsive:true,
+              maintainAspectRatio:true,
+              plugins: {
+                legend: {
+                  display: true,
+                  position: 'right',
+                  rtl: false, 
+                  labels: {
+                    usePointStyle: true,
+                    pointStyle: 'circle',
+                    // padding: 20,
+                    padding:20
+                  }
+                },
+
+              }
+            }
+      
+            }
+            />
+            {selected ==='진료' || btn1 === true?
+            <div>
+              <div className='dough-text' >총 {sum}회</div>
+              <div className='chartDough-button'>
+                <button style={{width:'83px'}} className={btn===1?'chart-button selectBtn':'chart-button'} onClick={()=>selectButton(1)}>진료과별</button>
+                <button style={{width:'63px'}} className={btn===2?'chart-button selectBtn':'chart-button'} onClick={()=>selectButton(2)}>질환별</button>
+              </div>
+            </div>
+            :''}
+      </div>
+    </div>
+
+  )
+}
+
+export default VideoAnalysis
+
+
+
+
+
+  // //chart 내부 총 합 function
+  // const drawInnerText = (chart) => {
+  //     let ctx = chart.ctx;
+  //     ctx.restore();
+  //     const fontSize = (chart.height / 270).toFixed(2);
+  //     ctx.font = fontSize + 'em sans-serif';
+  //     ctx.textBaseline = 'middle';
+  //     const dataArrValues = chart.config._config.data.datasets[0].data;
+  //     const result = dataArrValues.reduce(function add(sum, currValue) {
+  //       return sum+currValue;
+  //     }, 0);
+  //     let text = '총 ' + result + '회'
+  //       // chart.tooltip._active.length > 0
+  //       //   ? `${Math.round(
+  //       //       (dataArrValues[chart.tooltip._active[0].datasetIndex] /
+  //       //         dataArrValues.reduce((a, b) => a + b)) *
+  //       //         100
+  //       //     )}%`
+  //       //   : `${Math.round(
+  //       //       (dataArrValues[0] / dataArrValues.reduce((a, b) => a + b)) * 100
+  //       //     )}%`;
+  //     // let textX = Math.round((chart.width - ctx.measureText(text).width) / 2);
+  //     let textX = 42;
+  //     let textY = 125;
+  //     ctx.fillText(text, textX, textY);
+  //     ctx.fillStyle = '#6A7997';
+  //     ctx.save();
+  //   };
+  
+
+
+              // plugins = {[
+            //   {
+            //     beforeDraw: function(chart)
+            //     {
+            //       drawInnerText(chart);
+            //     },
+                
+            //   },
+            // ]} 
+
+
+            //진료과별
 export const dataDepartment = {
 
   labels: ['피부과', '내과', '성형외과','외과', '이비인후과', '기타'],
   datasets: [
     {
-      label:' ',
       data: [30,22,20,14,10,4],
       backgroundColor: [
         'rgba(51, 97, 255, 1.0)',
@@ -40,7 +174,7 @@ export const dataDisease = {
   labels: ['피부과', '내과', '성형외과','외과', '이비인후과', '기타'],
   datasets: [
     {
-      label:' ',
+      // label:' ',
       data: [30,22,20,14,10,4],
       backgroundColor: [
         'rgba(51, 97, 255, 1.0)',
@@ -62,124 +196,3 @@ export const dataDisease = {
     },
   ],
 };
-
-const VideoAnalysis = () => {
-
-
-  const [selected, setSelected] = useState(false);
-  const [btn,setBtn] = useState(1);
-    const handleSelect = (e) => {
-
-
-      if(e.target.value === 'true'){
-        setSelected(true)
-      }else{
-        setSelected(false)
-      }
-  };
-
-  const selectButton = (num) =>{
-    setBtn(num)
-  }
-
-  //chart 내부 총 합 function
-  const drawInnerText = (chart) => {
-      let ctx = chart.ctx;
-      ctx.restore();
-      const fontSize = (chart.height / 270).toFixed(2);
-      ctx.font = fontSize + 'em sans-serif';
-      ctx.textBaseline = 'middle';
-      const dataArrValues = chart.config._config.data.datasets[0].data;
-      // console.log(dataArrValues.length)
-      const result = dataArrValues.reduce(function add(sum, currValue) {
-        return sum+currValue;
-      }, 0);
-      let text = '총 ' + result + '회'
-        // chart.tooltip._active.length > 0
-        //   ? `${Math.round(
-        //       (dataArrValues[chart.tooltip._active[0].datasetIndex] /
-        //         dataArrValues.reduce((a, b) => a + b)) *
-        //         100
-        //     )}%`
-        //   : `${Math.round(
-        //       (dataArrValues[0] / dataArrValues.reduce((a, b) => a + b)) * 100
-        //     )}%`;
-      // let textX = Math.round((chart.width - ctx.measureText(text).width) / 2);
-      let textX = 42;
-      let textY = 125;
-      // console.log(textX,textY);
-      ctx.fillText(text, textX, textY);
-      ctx.fillStyle = '#6A7997';
-      ctx.save();
-    };
-  
-  return (
-    <div className='video-analysis'>
-      <div className='title'>
-        <div className='analysis-title'>
-        조회수 분석
-        </div>
-        {selected?"":<div className='chartDough-button'>
-          <button style={{width:'83px'}} className={btn===1?'chart-button selectBtn':'chart-button'} onClick={()=>selectButton(1)}>진료과별</button>
-          <button style={{width:'63px'}} className={btn===2?'chart-button selectBtn':'chart-button'} onClick={()=>selectButton(2)}>질환별</button>
-        </div>}
-        <Dropdown className='video-dropdown'
-            options={['진료', '비진료']}
-            value={'진료'}
-            onChange={(value) => console.log('change!', value)}
-            onSelect={(value) => console.log('selected!', value)} // always fires once a selection happens even if there is no change
-            onClose={(closedBySelection) => console.log('closedBySelection?:', closedBySelection)}
-            onOpen={() => console.log('open!')}
-          />
-      </div>
-
-      <div className='doughnut'>
-      <Doughnut 
-          data={dataDepartment}
-          options = {{
-            layout: {
-              padding: {
-                bottom: 0
-              },
-              
-            },
-            responsive:true,
-            maintainAspectRatio:true,
-            plugins: {
-              legend: {
-                display: true,
-                position: 'right',
-                rtl: false, 
-                labels: {
-                  usePointStyle: true,
-                  pointStyle: 'circle',
-                  padding: 20,
-                }
-              },
-
-            }
-          }
-    
-          }
-          plugins = {[
-            {
-              beforeDraw: function(chart)
-              {
-                drawInnerText(chart);
-              },
-              beforeInit: function(chart, options) {
-                chart.legend.afterFit = function() {
-                  this.width = this.width + 100;
-                }
-              }
-              
-            },
-          ]} />
-  
-      </div>
-    </div>
-
-  )
-}
-
-export default VideoAnalysis
